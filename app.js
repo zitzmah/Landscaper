e3160290b5680f660fa5b7249f66fa08b20add73
zitzmah <haleyzitzmann@gmail.com>
2023-11-08 05:24:00 -0700

    landscaper: user can use teeth to cut grass

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("Hello")
+
+//You are starting a landscaping business, but all you have are your teeth.
+let day = 0;
+let money = 0;
+const tools = ["teeth"]
+//Using just your teeth, you can spend the day cutting lawns and make $1. You can do this as much as you want.
+if (tools.contains("teeth")) {
+    money = day * 1;
+}
