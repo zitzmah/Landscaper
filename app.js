c1a5d6793d436047ed4d95da616076062c5f98ae
zitzmah <haleyzitzmann@gmail.com>
2023-11-08 05:33:09 -0700

    landscaper: user can use scissors to cut grass

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ let tool = "teeth"
 
 //Using just your teeth, you can spend the day cutting lawns and make $1. You can do this as much as you want.
 if (tool === "teeth") {
-    money = day * 1;
+    money = money + day;
 }
 
 //At any point, if you are currently using your teeth, you can buy a pair of rusty scissors for $5. You can do this once, assuming you have enough money.
 if (tool === "teeth" && money >= 5 && !toolbox.includes("scissors")) {
     toolbox.push("scissors")
+}
+
+//Using the rusty scissors, you can spend the day cutting lawns and make $5. You can do this as much as you want.
+if (tool === scissors) {
+    money = money + (day * 5)
 }
