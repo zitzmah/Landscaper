dc4771f3c7d9147dda27c2062d46a61cfd228354
zitzmah <haleyzitzmann@gmail.com>
2023-11-08 05:29:37 -0700

    landscaper: user can buy scissors

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ console.log("Hello")
 //You are starting a landscaping business, but all you have are your teeth.
 let day = 0;
 let money = 0;
-const tools = ["teeth"]
+const toolbox = ["teeth"]
+let tool = "teeth"
+
 //Using just your teeth, you can spend the day cutting lawns and make $1. You can do this as much as you want.
-if (tools.contains("teeth")) {
+if (tool === "teeth") {
     money = day * 1;
+}
+
+//At any point, if you are currently using your teeth, you can buy a pair of rusty scissors for $5. You can do this once, assuming you have enough money.
+if (tool === "teeth" && money >= 5 && !toolbox.includes("scissors")) {
+    toolbox.push("scissors")
 }
