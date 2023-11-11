311825aec8d4413a37a9596a161b1f327cd4a7c8
zitzmah <haleyzitzmann@gmail.com>
2023-11-11 06:31:22 -0700

    rearranged code, consolidated functions

--- app.js
+++ app.js
@@ -7,51 +7,45 @@ let tool;
 const start = () => {
     tool = "teeth"
     toolbox.push(tool)
-    money = 20;
+    money = 0;
+    showStatus()
     askForAction();
 }
 
 const showStatus = () => {
-    alert("You have $" + money + " and have worked " + day + " days")
-}
-
-const askForAction = () => {
-    showStatus();
-    const choice = prompt("What do you want to do?", "shop / work / restart");
-    if (choice === "shop") {
-        
-    } else if (choice === "work") {
-        work()
-    } else if (choice === "restart") {
-        start()
-    }
+    alert("You have $" + money + " and you are using " + tool + " to cut grass.")
 }
 
 const work = () => {
     //Using just your teeth, you can spend the day cutting lawns and make $1. You can do this as much as you want.
     if (tool === "teeth") {
         money = money + 1;
-        console.log("You made $1!")
+        alert("You made $1! Now you have $" + money)
+        askForAction()
     } else if (tool === "scissors") { //Using the rusty scissors, you can spend the day cutting lawns and make $5. You can do this as much as you want.
         money = money + 5
-        console.log("You made $5!")
+        alert("You made $5! Now you have $" + money)
+        askForAction()
     } else if (tool === "lawnmower") {//Using the old-timey push lawnmower, you can spend the day cutting lawns and make $50. You can do this as much as you want.
         money = money + 50
-        console.log("You made $50!")
+        alert("You made $5! Now you have $" + money)
+        askForAction()
     } else if (tool === "Fancy Lawnmower") {//Using the the fancy battery-powered lawnmower, you can spend the day cutting lawns and make $100. You can do this as much as you want.
         money = money + 100
-        console.log("You made $100!")
+        alert("You made $5! Now you have $" + money)
+        askForAction()
     } else if (tool === "students") {//Using the the team of starving students, you can spend the day cutting lawns and make $250. You can do this as much as you want.
         money = money + 250
-        console.log("You made $250!")
+        alert("You made $5! Now you have $" + money)
+        askForAction()
     } else if (toolbox.includes("students") && money >= 1000) {//You win the game when you have a team of starving students and $1000. In this situation, send a message to the user telling them, they've won.
-        console.log("You Win!")
+        alert("You Win!")
     }
 }
 
 //At any point, if you are currently using your teeth, you can buy a pair of rusty scissors for $5. You can do this once, assuming you have enough money.
 const buyScissors = () => {
-    if (tool === "teeth" && money >= 5 && !toolbox.includes("scissors")) {
+    if (money >= 5 && !toolbox.includes("scissors")) {
         toolbox.push("scissors")
         tool = "scissors"
     } else {
@@ -62,7 +56,7 @@ const buyScissors = () => {
 
 //At any point, if you are currently using rusty scissors, you can buy an old-timey push lawnmower for $25. You can do this once, assuming you have enough money.
 const buyLawnmower = () => {
-    if (tool === "scissors" && money >= 25 && !toolbox.includes("lawnmower")) {
+    if (money >= 25 && !toolbox.includes("lawnmower")) {
         toolbox.push("lawnmower")
         tool = "lawnmower"
     } else {
@@ -73,7 +67,7 @@ const buyLawnmower = () => {
 
 //At any point, if you are currently using the old-timey push lawnmower, you can buy a fancy battery-powered lawnmower for $250. You can do this once, assuming you have enough money.
 const buyFancyLawnmower = () => {
-    if (tool === "lawnmower" && money >= 250 && !toolbox.includes("Fancy Lawnmower")) {
+    if (money >= 250 && !toolbox.includes("Fancy Lawnmower")) {
         toolbox.push("Fancy Lawnmower")
         tool = "Fancy Lawnmower"
     } else {
@@ -84,7 +78,7 @@ const buyFancyLawnmower = () => {
 
 //At any point, if you are currently using the fancy battery-powered lawnmower, you can hire a team of starving students for $500. You can do this once, assuming you have enough money.
 const hireStudents = () => {
-    if (tool === "Fancy Lawnmower" && money >= 500 && !toolbox.includes("students")) {
+    if (money >= 500 && !toolbox.includes("students")) {
         toolbox.push("students")
         tool = "students"
     } else {
@@ -92,3 +86,37 @@ const hireStudents = () => {
     }
     askForAction();
 }
+
+const shop = () => {
+    if (tool === "teeth") {
+        buyScissors()
+    } else if (tool === "scissors") {
+        buyLawnmower()
+    } else if (tool === "lawnmower") {
+        buyFancyLawnmower()
+    } else if (tool === "Fancy Lawnmower") {
+        hireStudents()
+    } else if (toolbox.includes("students") && money > 1000) {
+        console.log("keep working")
+        askForAction()
+    } else if (toolbox.includes("students") && money >= 1000) {
+        console.log("You win!")
+    }
+}
+
+const askForAction = () => {
+    const choice = prompt("What do you want to do?", "shop / work / restart");
+    if (choice === "shop") {
+        shop()
+        showStatus()
+    } else if (choice === "work") {
+        work()
+        showStatus()
+    } else if (choice === "restart") {
+        start()
+    }
+}
+
+alert('Welcome to the landscaper game');
+start()
+askForAction()
